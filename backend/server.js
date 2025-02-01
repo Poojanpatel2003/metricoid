e9e90ce2fe5d32a63427d96b11725412cd0b5619
Poojanpatel2003 <144653133+Poojanpatel2003@users.noreply.github.com>
2025-02-01 15:11:20 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(() => console.error(err));
+const userSchema = new mongoose.Schema({
+  name: String,
+  skills: [String],
+  Hobbies: [String],
+  Passions: [String],
+  academicBackground: String,
+});
+const User = mongoose.model("User", userSchema);
+app.get("/api/user", async (req, res) => {
+  const user = await User.findOne();
+  res.json(user);
+});
+app.post('/api/user',async(req,res)=>{
+    try {
+        const user=new User(req.body);
+        await user.save();
+        res.json(201).json({message:"added successfully",user})
+    } catch (error) {
+        console.log(error);
+        res.json(201).json({message:"failed to add"})
+    }
+})
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server is runnig on ${port}`));
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+const Home = () => {
+    const [user,setUser]=useState(null);
+    useEffect(()=>{
+        axios.get('http://localhost:5001/api/user').then(response=>setUser(response.data)).catch(error=>console.log(error))
+    },[])
+    return (
+        <div>
+            {user ? (
+                <div>
+                    <h1>Name: {user.name}</h1>
+                    <p>Skills: {user.skills}</p>
+                    <p>Hobbies: {user.Hobbies}</p>
+                    <p>Passions: {user.Passions}</p>
+                    <p>academicBackground: {user.academicBackground}</p>
+                    </div>
+            )
+        : <p>Loading..</p>
+        }
+        </div>
+    );
+}
+
+export default Home;
